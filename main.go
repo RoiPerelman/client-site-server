e5095638bec8f3467e1925b17e7e6a280b3c9afa
Roi Perelman <roi@dynamicyield.com>
2018-06-18 19:32:50 +0300

    add build

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
+	"github.com/gorilla/mux"
 	"github.com/roiperelman/client-site-server/models"
 	"github.com/roiperelman/client-site-server/handlers"
 )
@@ -15,33 +15,16 @@ func main() {
 	r.HandleFunc("/api/user/authorize", handlers.AuthorizeUser).Methods("GET")
 	r.HandleFunc("/api/user/signup", handlers.SignupUser).Methods("POST")
 	r.HandleFunc("/api/user/login", handlers.LoginUser).Methods("POST")
+	r.PathPrefix("/static").Handler(http.FileServer(http.Dir("./build")))
+	r.PathPrefix("/").HandlerFunc(staticFileHandler("./build/index.html"))
+
 	http.ListenAndServe(":1111", r)
 }
 
-//func JsonResponse(response interface{}, w http.ResponseWriter) {
-//
-//	json, err := json.Marshal(response)
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	w.WriteHeader(http.StatusOK)
-//	w.Header().Set("Content-Type", "application/json")
-//	w.Write(json)
-//}
+func staticFileHandler(filePath string) func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filePath)
+	}
 
-//err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(user.Password))
-//if err != nil {
-//	http.Error(w, err.Error(), 500)
-//}
-
-//timestamp := claims["timestamp"]
-//if t, ok := timestamp.(float64); ok {
-//	duration := int64(time.Now().Sub(time.Unix(int64(t), 0)))
-//	maximumDuration := int64(3600 * time.Second)
-//	fmt.Println(maximumDuration - int64(duration))
-//} else {
-//	fmt.Println("err")
-//	fmt.Println(time.Now().Unix())
-//}
+	return http.HandlerFunc(fn)
+}
