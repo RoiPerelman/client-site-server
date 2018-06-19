a19aa0605b2eb48c3562a94acfca87278d30a70a
Roi Perelman <roi@dynamicyield.com>
2018-06-19 09:39:35 +0300

    change to postgres

--- models/db.go
+++ models/db.go
@@ -1,16 +1,21 @@
 package models
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	//_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"database/sql"
 	"log"
+	"os"
+	"fmt"
 )
 
 var db *sql.DB
 
 func InitDB() () {
 	var err error
-	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/client_site_server_development")
+	//db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/client_site_server_development")
+	fmt.Printf("DATABASE_URL CONNECTION TO " + os.Getenv("DATABASE_URL"))
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -20,6 +25,7 @@ func InitDB() () {
 		log.Panic(err)
 	}
 }
+
 
 //func Close() {
 //	db.Close()
