c5c4fa2d42c5863aa5a38cfd3c7945dac72aa332
Roi Perelman <roi@dynamicyield.com>
2018-07-16 11:26:42 +0300

    finish jsCode handler test

--- handlers/jsCode_test.go
+++ handlers/jsCode_test.go
@@ -1,33 +1,97 @@
 package handlers
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/assert"
 	"context"
-	"github.com/roiperelman/client-site-server/models"
-	"strings"
+	"errors"
 	"fmt"
+	"net/http"
+	"strings"
+	"net/http/httptest"
+	"github.com/roiperelman/client-site-server/models"
 )
 
-type mockDBUserJSStore struct{
-	models.DBUserJSStore
+// the following type and function are to be used with testify
+type mockDBUserJSStoreWithTestify struct{
+	mock.Mock
 }
 
-type mockDb struct {}
-
-func (mockDb *mockDb) UpdateJSCode(id int, jsCode string) error {
-	return nil
+func (m *mockDBUserJSStoreWithTestify) UpdateJSCode(id int, jsCode string) error {
+	args := m.Called(id, jsCode)
+	return args.Error(0)
 }
 
-func TestEndpoints(t *testing.T) {
-	t.Run("test jsCode endpoint", func(t *testing.T) {
+func TestUpdateJSCode(t *testing.T) {
+	const UserId = "UserId"
+	const DBStore = "DBStore"
+
+	tests := map[string]struct {
+		DBUpdateJSCodeResponse error
+		contexts []string
+		want string
+	}{
+		"DBStore returns an error": {
+			DBUpdateJSCodeResponse: errors.New("mockDBStore error"),
+			contexts: []string{UserId, DBStore},
+			want: "mockDBStore error\n",
+		},
+		"UserId doesnt exist in request.context": {
+			DBUpdateJSCodeResponse: nil,
+			contexts: []string{DBStore},
+			want: "update JS code failed\n",
+		},
+		"DBStore doesnt exist in request.context": {
+			DBUpdateJSCodeResponse: nil,
+			contexts: []string{UserId},
+			want: "db connection failed\n",
+		},
+		"Successful run": {
+			DBUpdateJSCodeResponse: nil,
+			contexts: []string{UserId, DBStore},
+			want: "{\"jsCode\":\"console.log('JSCode Test')\"}\n",
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			jsonStr := fmt.Sprintf("{\"jsCode\":\"console.log('JSCode Test')\"}\n")
+			request, _ := http.NewRequest(http.MethodPost, "/api/user/updateJSCode", strings.NewReader(jsonStr))
+			response := httptest.NewRecorder()
+
+			// add UserId context
+			//if (test.contexts)
+			for _, testContext := range test.contexts {
+				switch testContext {
+					case UserId:
+						userId := 1
+						ctx := context.WithValue(request.Context(), "UserId", userId)
+						request = request.WithContext(ctx)
+					case DBStore:
+						testDBUserJSStore := new(mockDBUserJSStoreWithTestify)
+						testDBUserJSStore.
+							On("UpdateJSCode", mock.Anything, mock.Anything).
+							Return(test.DBUpdateJSCodeResponse)
+						ctx := context.WithValue(request.Context(), "DBStore", testDBUserJSStore)
+						request = request.WithContext(ctx)
+				}
+			}
+
+			UpdateJSCode(response, request)
+
+			got := response.Body.String()
+			assert.Equal(t, got, test.want, "they should be equal")
+		})
+	}
+
+	t.Run("Example of a test with mocking the old fashioned way", func(t *testing.T) {
 		jsonStr := fmt.Sprintf("{\"jsCode\":\"console.log('JSCode Test')\"}\n")
 		request, _ := http.NewRequest(http.MethodPost, "/api/user/updateJSCode", strings.NewReader(jsonStr))
 		response := httptest.NewRecorder()
 
-		// add contexts
-		userId := 16
+		// add UserId context
+		userId := 1
 		ctx := context.WithValue(request.Context(), "UserId", userId)
 		request = request.WithContext(ctx)
 
@@ -39,15 +103,17 @@ func TestEndpoints(t *testing.T) {
 
 		got := response.Body.String()
 		want := "{\"jsCode\":\"console.log('JSCode Test')\"}\n"
-		print(want)
-		assertResponseBody(t, got, want)
+		assert.Equal(t, got, want, "they should be equal")
 	})
 }
 
-func assertResponseBody(t *testing.T, got, want string) {
-	t.Helper()
-	println(strings.Compare(got,want))
-	if got != want {
-		t.Errorf("response body is wrong, got '%s' want '%s'", got, want)
-	}
+//the following 2 types and function are used for mocking the old fashioned way
+type mockDBUserJSStore struct {
+	models.DBUserJSStore
+}
+
+type mockDb struct {}
+
+func (mockDb *mockDb) UpdateJSCode(id int, jsCode string) error {
+	return nil
 }
